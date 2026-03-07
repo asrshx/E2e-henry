41d46585e764d80f231c94a35bd512a4c6f5eee7
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2026-03-07 13:24:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,26 @@ import json
 import urllib.parse
 from pathlib import Path
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=chrome_options
+)
 import database as db
 import requests
 
 st.set_page_config(
     page_title="HENRY-X AUTOMATION PANEL",
-    page_icon="ðŸ¥µ",
+    page_icon="HENRY",
     layout="wide",
     initial_sidebar_state="expanded"
 )
