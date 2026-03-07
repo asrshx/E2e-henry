612ee724497900a48404b3d99bfc3073e06c723c
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2026-03-07 16:11:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,28 +13,16 @@ import requests
 
 # Selenium imports
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 
-import database as db
-
-
-# ---------- Chrome Setup ----------
 chrome_options = Options()
-
-chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--window-size=1920,1080")
 
-# Render / Linux environment ke liye chromium path
 chrome_options.binary_location = "/usr/bin/chromium"
 
-# Driver path
 service = Service("/usr/bin/chromedriver")
 
 driver = webdriver.Chrome(
